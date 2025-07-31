5758f4b9fd7c937ef916e25adcdc294f333a7fba
birdloverr <chocolatefactory36@centurylink.net>
2025-07-31 15:10:06 -0400

    Update askbox_result.php

--- finalsite/askbox_result.php
+++ finalsite/askbox_result.php
@@ -7,24 +7,98 @@ head
   ol, ul { 
      list-style-type none;
       }
-  body {  
-    background-color #faf2e4;
-    margin 0 10%;
-    font-family sans-serif;
+  .eb-garamond-<uniquifier> {
+  font-family: "EB Garamond", serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
+}
+.monsieur-la-doulaise-regular {
+  font-family: "Monsieur La Doulaise", cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+body {
+  background-color: #F8F6F3;
+  margin: 10 10%;
+  font-family: serif;
+  color: #716154
   }
-  h1 {
-    text-align center;
-    font-family serif;
-    font-weight normal;
-    text-transform uppercase;
-    border-bottom 1px solid #57b1dc;
-    margin-top 30px;
-  }
+h1 {
+  text-align: center;
+  font-family: serif;
+  font-weight: bold;
+  font-size:3em;
+  margin-top: 50px;
+  margin-left: 700px;
+  margin-right: 700px;
+  color: #EA7B99;
+  background-color: #F9D7DD;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
+}
+h2 {
+  color: #F1B1C2;
+  font-size: 1.5em;
+  margin-top: 50px;
+}
+h3 {
+  color: #F8CBD8;
+  font-size: 1.2em;
+  font-weight: light;
+}
+div {
+  margin-left: 200px;
+}
+img.responsive-img {
+      width: 100%;
+      max-width: 1200px;
+      height: auto;
+      display: block;
+      margin: 1em auto;
+      border-radius: 8px;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+    }
+ nav ul {
+      list-style: none;
+      padding: 0;
+      margin: 20px 0;
+      text-align: center;
+      background-color: #FFFFFF;
+      margin-left: 875px;
+      margin-right: 875px;
+    }
 
-  h2 {
-    color #d1633c;
-    font-size 1em;
-  }
+    nav li {
+      display: inline;
+      margin: 0 15px;
+    }
+
+    nav a {
+      text-decoration: none;
+      color: #FABFCE;
+      font-weight: bold;
+      font-size: 1.1em;
+    }
+
+    nav a:hover {
+      color: #a8492e;
+    }
+  .button {
+      display: block;
+      width: fit-content;
+      margin: 30px auto;
+      padding: 12px 24px;
+      background-color: #F690A0;
+      color: white;
+      font-weight: italic;
+      font-size: 1.2em;
+      border-radius: 8px;
+      text-align: center;
+    }
+
+    .button:hover {
+      background-color: #E8BDC5;
+    }
   p.disclaimer { 
     border-top 1px solid #d1633c; 
     padding-top 1em;
